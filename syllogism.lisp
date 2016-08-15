b8bcac75454b9ce6d6f74d82922999e15fa57ead
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-08-16 00:16:19 +0300

    Struct declarations, operations

--- syllogism.lisp
+++ syllogism.lisp
@@ -10,15 +10,16 @@
     ;; TODO include an option to quit
     (cond ((null input)
            (princ "\nError: invalid input."))
-          ((fact-p input)
-           (add-fact (fact input) env)
+          ((assertion-p input)
+           (add-stmt (assertion-stmt input) env)
            (princ "\nok"))
-          ((setf proof (prove (query input) env))
+          ((setf proof (prove (query-stmt input) env))
            (format t "~%Yes.~{~^~%~S~} q.e.d."
              (mapcar #'output-fact (steps proof))))
           (T (princ "\nNo.")))) ;perhaps describe why the statement is wrong, or where a contradiction occurred.
   (syllogism-repl env))
   
+;; returns either an assertion or a query, and NIL for invalid input
 (defun parse-input (str)
   "Parse a statement inputted to the syllogism solver."
   NIL)
@@ -27,26 +28,13 @@
   "Convert fact into a string to be printed."
   NIL)
 
-(defun fact-p (input)
-  "Return T if the input describes a fact."
-  NIL)
-  
-(defun fact (input)
-  "Return the fact in an input, if it describes one."
-  NIL)
-
-(defun query-p (input)
-  "Return T if the input describes a query."
-  NIL)
-  
-(defun query (input)
-  "Return the query in an input, if it describes one."
-  NIL)
+(defstruct assertion "Wrapper struct for an assertion command." stmt)
+(defstruct query "Wrapper struct for a query command." stmt)
   
 ;;; Core model
 
-(defun prove (query &optional (env *toplevel-env*))
-  "Return a proof of QUERY using facts in ENV, if one exists; returns NIL otherwise."
+(defun prove (stmt &optional (env *toplevel-env*))
+  "Return a proof of the given statement using facts in ENV, if one exists; returns NIL otherwise."
   NIL)
   
 (defun steps (proof)
@@ -55,9 +43,24 @@
   
 ;;; Data structures
 
-(defparameter *toplevel-env* NIL)
+(defstruct env "Contains an alist mapping each subject to the statements they are a subject of." alist)
 
-(defun add-fact (fact &optional (env *toplevel-env*))
-  "Add fact to environment."
-  NIL)
-  
+(defstruct (stmt (:type list)) 
+  "A syllogistic statement that binds a subject to a predicate based on the given type (either A, E, I or O)." 
+  sub type pred)
+
+(defconstant inference-rules
+  '((A (A A 1))
+    (E (E A 1) (A E 2) (E A 2) (A E 4))
+    (I (A I 1) (A I 3) (I A 3) (I A 4))
+    (O (E A 1) (E I 1) (A E 2) (E A 2) (A O 2) (E I 2)
+       (E A 3) (O A 3) (E I 3) (A E 4) (E A 4) (E I 4)))
+  "Maps types of conclusions to the different combinations of major and minor premise types and figures that prove it.")
+
+(defparameter *toplevel-env* (make-env) "Default environment for statements.")
+
+(defun add-stmt (stmt &optional (env *toplevel-env*) &aux assoc)
+  "Add statement to environment."
+  (if (setf assoc (assoc (stmt-sub stmt) (env-alist env)))
+      (push (cdr stmt) (cdr assoc))
+      (push (list (car stmt) (cdr stmt)) (env-alist env))))
